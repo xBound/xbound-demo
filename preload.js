52be24f807f4f6705994a8348caac1e47d29b1ff
Mihail Stoian <stoianmmihail@yahoo.com>
2026-03-27 12:45:00 +0100

    udpate title

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('xbound', {
-  appName: 'xBound VLDB26 Demo',
+  appName: '',
   loadPrecomputedEstimates: (benchmark, xboundParams) => ipcRenderer.invoke('xbound:load-precomputed-estimates', benchmark, xboundParams),
   loadWorkloadQueries: (benchmark) => ipcRenderer.invoke('xbound:load-workload-queries', benchmark),
   estimateCustomQuery: (benchmark, sql, xboundParams, queryTag) => ipcRenderer.invoke('xbound:estimate-custom-query', benchmark, sql, xboundParams, queryTag)
